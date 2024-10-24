cb276f994d0be0286127a6ed8f5c99df3222748c
Daniele Della Cioppa <daniele.dellacioppa@gmail.com>
2024-10-24 16:39:53 +0200

    logo ascii

--- AkhterAirPlayReceiver/app/src/main/java/com/cjx/airplayjavademo/compose/VideoDisplayComposable.kt
+++ AkhterAirPlayReceiver/app/src/main/java/com/cjx/airplayjavademo/compose/VideoDisplayComposable.kt
@@ -9,6 +9,16 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import android.view.SurfaceHolder
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.cjx.airplayjavademo.tools.LogRepository
 
 @Composable
@@ -40,10 +50,38 @@ fun VideoDisplayComposable(
 fun LogDisplayComposable() {
     val logMessages = remember { LogRepository.getLogs() }
 
+    // Layout principale che mostra il logo fisso in alto e la LazyColumn che scorre
+    Column(modifier = Modifier.fillMaxSize()) {
 
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
-        items(logMessages.size) { index ->
-            BasicText(text = logMessages[index])
+        // Mostra il logo ASCII sempre in testa
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = logoAscii,
+                style = TextStyle(fontSize = 6.sp, color = Color.Gray) // Font molto piccolo
+            )
+        }
+
+        // La LazyColumn con i log che può essere scrollata
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(logMessages.size) { index ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    // Mostra i log con uno stile normale
+                    Text(
+                        text = logMessages[index],
+                        style = TextStyle(fontSize = 16.sp) // Font normale per i log
+                    )
+                }
+            }
         }
     }
 }
