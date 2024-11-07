c67a1475354a041e30261382c7f4ba178b57f9c3
Daniele Della Cioppa <daniele.dellacioppa@gmail.com>
2024-11-07 14:18:46 +0000

    ui

--- AkhterAirPlayReceiver/app/src/main/java/com/cjx/airplayjavademo/compose/LogDisplayComposable.kt
+++ AkhterAirPlayReceiver/app/src/main/java/com/cjx/airplayjavademo/compose/LogDisplayComposable.kt
@@ -4,17 +4,21 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -31,6 +35,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.cjx.airplayjavademo.tools.LogEntry
 import com.cjx.airplayjavademo.tools.LogRepository
 import com.cjx.airplayjavademo.ui.theme.Gray40
 
@@ -146,124 +151,35 @@ fun LogDisplayComposable(
                 // LazyRow per i pulsanti sotto il logo
                 androidx.compose.foundation.lazy.LazyRow(
                     modifier = Modifier
-                        .fillMaxWidth()
+//                        .fillMaxWidth()
                         .padding(vertical = 8.dp),
                     horizontalArrangement = Arrangement.spacedBy(8.dp)
                 ) {
+                    val paddingValues = PaddingValues(2.dp)
                     items(buttons.size) { index ->
                         val (label, action) = buttons[index]
-                        Button(onClick = action) {
+                        Button(
+                            onClick = action,
+                            modifier = Modifier.size(60.dp, 25.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                backgroundColor = Color(0xFF464545),
+                                contentColor = Color.Gray
+                            ),
+                            contentPadding = paddingValues,
+                        ) {
                             Text(
                                 label,
-                                style = TextStyle(fontSize = 12.sp, fontFamily = minecraftFont)
+//                                modifier = Modifier.size(20.dp, 6.dp),
+                                color = Color.Gray,
+                                style = TextStyle(fontSize = 8.sp, fontFamily = minecraftFont)
                             )
                         }
                     }
                 }
             }
-
-
         }
-
         // La LazyColumn con i log che può essere scrollata
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp) // Stacca la LazyColumn dai bordi
-                .clip(RoundedCornerShape(6.dp)) // Angoli arrotondati
-                .background(
-                    Brush.verticalGradient(
-                        colors = listOf(
-                            Color(0x55AAEA00),
-                            Color(0xFF464545)
-                        )
-                    )
-                ),
-            state = listState
-        ) {
-            items(logMessages.size) { index ->
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    verticalAlignment = Alignment.Top,
-                    horizontalArrangement = Arrangement.Start
-                ) {
-
-                    // Imposta larghezza fissa per il punto
-                    Box(
-                        modifier = Modifier
-                            .width(50.dp) // Fissare la larghezza per la pallina
-                            .height(25.dp) // Fissare l'altezza per la pallina
-                            .align(Alignment.CenterVertically)
-                    ) {
-                        Text(
-                            text = logMessages[index].time, // Ora del log
-                            style = TextStyle(
-                                color = Color(0xFFFF6600), // Testo arancione
-                                fontSize = 12.sp,
-                                fontFamily = alata,
-                                shadow = Shadow(
-                                    color = Color.Black,
-                                    offset = Offset(2f, 2f),
-                                    blurRadius = 3f
-                                )
-                            ),
-                            textAlign = TextAlign.Center
-                        )
-                    }
-
-                    // Mostra il tag con testo rosso
-                    Text(
-                        text = logMessages[index].tag, // Tag del log
-                        modifier = Modifier
-                            .padding(start = 3.dp) // Margine per distanziare il testo dal punto
-                            .align(Alignment.CenterVertically),
-                        style = TextStyle(
-                            color = Color.Cyan,
-                            fontSize = 10.sp,
-                            fontFamily = FontFamily.Monospace, // Imposta font monospace
-                            shadow = Shadow(
-                                color = Color.Black,
-                                offset = Offset(2f, 2f),
-                                blurRadius = 3f
-                            )
-                        ),
-                        fontFamily = FontFamily.Monospace
-                    )
-
-                    // Mostra i log con testo giallo
-                    Text(
-                        text = logMessages[index].message, // Messaggio del log
-                        modifier = Modifier
-                            .padding(start = 3.dp) // Margine per distanziare il testo dal punto
-                            .align(Alignment.CenterVertically)
-                            .clip(RoundedCornerShape(3.dp))
-                            //sfondo sfumato trasparente
-                            .background(
-                                Brush.verticalGradient(
-                                    colors = listOf(
-                                        logMessages[index].type.color.copy(alpha = 0.3f),
-                                        Color.Transparent
-                                    )
-                                )
-                            )
-                        ,
-                        style = TextStyle(
-                            color = logMessages[index].type.color, // Colore del testo
-                            fontSize = 13.sp,
-                            fontFamily = FontFamily.Monospace, // Imposta font monospace
-//                            lineHeight = 4.sp,
-                            shadow = Shadow(
-                                color = Color.Black,
-                                offset = Offset(2f, 2f),
-                                blurRadius = 3f
-                            )
-                        ),
-                        fontFamily = FontFamily.Monospace
-                    )
-                }
-            }
-        }
+        LogColumn(listState, logMessages)
     }
 
     // Effettua lo scroll verso l'ultimo elemento quando la lista cambia
@@ -271,6 +187,8 @@ fun LogDisplayComposable(
         listState.animateScrollToItem(logMessages.size - 1)
     }
 }
+
+
 
 fun expandSpaces(original: String, sp: Int = 4): String {
 
