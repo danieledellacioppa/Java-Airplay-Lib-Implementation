1fef96feef654bb131d20b5c5d6aad03826d466d
Daniele Della Cioppa <daniele.dellacioppa@gmail.com>
2025-01-27 20:06:05 +0000

    little tweak with no meaning

--- AkhterAirPlayReceiver/app/src/main/java/com/cjx/airplayjavademo/compose/logdisplay/LogScaffoldContent.kt
+++ AkhterAirPlayReceiver/app/src/main/java/com/cjx/airplayjavademo/compose/logdisplay/LogScaffoldContent.kt
@@ -112,7 +112,7 @@ fun LogScaffoldContent(
                         verticalArrangement = Arrangement.Center
                     ){
                         Text(
-                            text = "AirPlay Receiver",
+                            text = "..:Testing AirPlay Receiver:..",
                             style = TextStyle(
                                 fontSize = 10.sp,
                                 color = Color.White.copy(alpha = 0.7f),
